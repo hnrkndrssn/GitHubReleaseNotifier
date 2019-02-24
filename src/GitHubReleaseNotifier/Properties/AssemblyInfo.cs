35f948e073a272e05bcdeef61d555326c1e1487b
Henrik Andersson <henrik@henrikandersson.org>
2019-02-24 17:45:27 +1000

    Add AssemblyInfo file

---
+++ src/GitHubReleaseNotifier/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GitHubReleaseNotifier")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Henrik Andersson")]
+[assembly: AssemblyProduct("GitHubReleaseNotifier")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8e17d5f8-b8e1-4f0d-b61c-7bffba995fdb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
